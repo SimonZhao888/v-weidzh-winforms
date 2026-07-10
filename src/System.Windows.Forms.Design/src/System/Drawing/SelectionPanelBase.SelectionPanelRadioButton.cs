e0ffb46541df060fcc17033d43bc706b02b6acb4
Simon Zhao (BEYONDSOFT CONSULTING INC) <v-weidzh@microsoft.com>
2026-07-10 10:09:41 +0800

    Handle feedback

--- src/System.Windows.Forms.Design/src/System/Drawing/SelectionPanelBase.SelectionPanelRadioButton.cs
+++ src/System.Windows.Forms.Design/src/System/Drawing/SelectionPanelBase.SelectionPanelRadioButton.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace System.Drawing.Design;
 
@@ -20,20 +21,39 @@ internal abstract partial class SelectionPanelBase
         {
             base.WndProc(ref m);
 
-            if (m.MsgInternal != PInvokeCore.WM_PAINT || !Focused || !ShowFocusCues)
-            {
-                return;
-            }
-
-            Rectangle focusBounds = ClientRectangle;
-            focusBounds.Inflate(-LogicalToDeviceUnits(3), -LogicalToDeviceUnits(3));
-            if (focusBounds.Width <= 0 || focusBounds.Height <= 0)
+            if (m.MsgInternal != PInvokeCore.WM_PAINT || !Focused || !Application.IsDarkModeEnabled)
             {
                 return;
             }
 
             using Graphics g = CreateGraphics();
+            Rectangle focusBounds = GetFocusBounds(g);
+            if (focusBounds.Width <= 0 || focusBounds.Height <= 0)
+            {
+                return;
+            }
+
             ControlPaint.DrawFocusRectangle(g, focusBounds, ForeColor, BackColor);
+        }
+
+        private Rectangle GetFocusBounds(Graphics graphics)
+        {
+            if (Application.RenderWithVisualStyles && VisualStyleRenderer.IsSupported)
+            {
+                VisualStyleElement element = Checked
+                    ? VisualStyleElement.Button.PushButton.Pressed
+                    : VisualStyleElement.Button.PushButton.Normal;
+
+                if (VisualStyleRenderer.IsElementDefined(element))
+                {
+                    VisualStyleRenderer renderer = new(element);
+                    return renderer.GetBackgroundContentRectangle(graphics, ClientRectangle);
+                }
+            }
+
+            Rectangle focusBounds = ClientRectangle;
+            focusBounds.Inflate(-SystemInformation.Border3DSize.Width, -SystemInformation.Border3DSize.Height);
+            return focusBounds;
         }
 
         protected override bool IsInputKey(Keys keyData) => keyData switch
